91ac16952e4f0b3757a614cc069a8f6e70e5fc2a
Andy Tong <ivsall2012@gmail.com>
2017-05-18 10:03:34 -0700

    loginVC UI ok

--- BloomKids/BKLoginVC.swift
+++ BloomKids/BKLoginVC.swift
@@ -8,18 +8,62 @@
 
 import UIKit
 
+let fakeEmail = "123@gmail.com"
+let fakePassword = "123"
+
 class BKLoginVC: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+    }
+    @IBAction func forgotPasswordTapped(_ sender: UIButton) {
+        print("forgotPasswordTapped")
     }
 
+    @IBAction func loginBtnTapped(_ sender: UIButton) {
+        print("loginBtnTapped")
+    }
+    @IBAction func loginFBTapped(_ sender: UIButton) {
+        print("loginFBTapped")
+    }
+    @IBAction func loginGoogleTapped(_ sender: UIButton) {
+        print("loginGoogleTapped")
+    }
     
 
 }
 
 
 //MARK:- Setups
-extension BKLoginVC {
+extension BKLoginVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let text = textField.text, text.characters.count != 0 else {
+            return false
+        }
+        
+        if textField === self.emailTextField {
+            self.passwordTextField.becomeFirstResponder()
+        }else{
+            authenticate(self.emailTextField.text!, password: self.passwordTextField.text!)
+        }
+        return true
+    }
     
+    func authenticate(_ email: String, password: String) {
+        if email == fakeEmail && password == fakePassword {
+            print("ok")
+        }else{
+            print("failed")
+        }
+    }
 }
+
+
+
+
+
+
